019712cc2813618ba57d22d0c84f8fd8a2b8d88a
GuanyuLin <49344274+GuanyuLin@users.noreply.github.com>
2022-08-31 00:23:46 -0400

    Update SearchContacts.php

--- LAMP Stack/LAMPAPI/SearchContacts.php
+++ LAMP Stack/LAMPAPI/SearchContacts.php
@@ -5,7 +5,7 @@
 	$searchResults = "";
 	$searchCount = 0;
 
-	$conn = new mysqli("localhost", "root", "go1Knights", "COP4331");
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
