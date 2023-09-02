27f51214df87c750d836b56ba5222254a5888b9e
SamElomari <90078380+SamElomari@users.noreply.github.com>
2023-09-02 17:05:04 -0400

    Update Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -7,7 +7,7 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	$conn = new mysqli("localhost", "APIMan", "WeLoveAPIMan"", "COP4331"); 	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
